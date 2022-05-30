e565ea67d6e014f21da45c88c9806f6acc57e016
henry cunha <henrycunh@gmail.com>
2022-05-30 10:34:04 -0300

    feat: add ohmyfetch package

--- src/global.ts
+++ src/global.ts
@@ -4,6 +4,7 @@ import kleur from 'kleur'
 import axios from 'axios'
 import fs from 'fs-extra'
 import YAML from 'yaml'
+import { $fetch } from 'ohmyfetch'
 import * as glob from 'globby'
 import { style } from 'kleur-template'
 import { io, read, write } from './io'
@@ -17,23 +18,28 @@ import {
 } from './index'
 
 export function registerGlobals() {
-    Object.assign(global, {
-        $,
-        argv,
-        cd,
-        kleur,
-        axios,
-        fs,
-        glob,
-        nothrow,
-        os,
-        path,
-        sleep,
-        YAML,
-        io,
-        read,
-        write,
-        template,
-        style
-    })
+    if (!global.__haxx__) {
+        Object.assign(global, {
+            $,
+            $fetch,
+            argv,
+            cd,
+            kleur,
+            axios,
+            fs,
+            glob,
+            nothrow,
+            os,
+            path,
+            sleep,
+            YAML,
+            io,
+            read,
+            write,
+            template,
+            style,
+        })
+    }
+
+    global.__haxx__ = true
 }
